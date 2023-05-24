ad0f1e8904d7a3152c40ef8059f5d004dd27a64e
joelhi <d.j.hilmersson@gmail.com>
2023-05-24 13:06:17 +0100

    final formatting

--- src/MeshGraphLib/Algorithms/LaplacianSmooth.cs
+++ src/MeshGraphLib/Algorithms/LaplacianSmooth.cs
@@ -3,58 +3,58 @@ using System.Collections.Generic;
 using MeshGraphLib.Core;
 namespace MeshGraphLib.Algorithms
 {
-	public class LaplacianSmooth
-	{
-		private HashSet<int> fixed_v;
-		private GraphXYZ vertex_graph; 
-		public LaplacianSmooth(GraphXYZ vertex_graph, IEnumerable<XYZ> fixed_points)
-		{
-			this.vertex_graph = vertex_graph;
-			fixed_v = new HashSet<int>();
+    public class LaplacianSmooth
+    {
+        private HashSet<int> fixed_v;
+        private GraphXYZ vertex_graph;
+        public LaplacianSmooth(GraphXYZ vertex_graph, IEnumerable<XYZ> fixed_points)
+        {
+            this.vertex_graph = vertex_graph;
+            fixed_v = new HashSet<int>();
 
-			foreach (XYZ p in fixed_points) { fixed_v.Add(vertex_graph.NodeIndex(p)); }
-		}
+            foreach (XYZ p in fixed_points) { fixed_v.Add(vertex_graph.NodeIndex(p)); }
+        }
 
-		public GraphXYZ Smooth(int iterations)
-		{
-			XYZ[] smooth_nodes = new XYZ[vertex_graph.NodeCount];
+        public GraphXYZ Smooth(int iterations)
+        {
+            XYZ[] smooth_nodes = new XYZ[vertex_graph.NodeCount];
 
-			XYZ[] nodes = vertex_graph.GetNodes();
+            XYZ[] nodes = vertex_graph.GetNodes();
 
-			for (int iter = 0; iter < iterations; iter++)
-			{
-				for (int i = 0; i < vertex_graph.NodeCount; i++)
-				{
-					if(fixed_v.Contains(i))
-					{
-						smooth_nodes[i] = nodes[i];
-						continue;
-					}
+            for (int iter = 0; iter < iterations; iter++)
+            {
+                for (int i = 0; i < vertex_graph.NodeCount; i++)
+                {
+                    if (fixed_v.Contains(i))
+                    {
+                        smooth_nodes[i] = nodes[i];
+                        continue;
+                    }
 
-					HashSet<int> connected = vertex_graph.GetConnectedNodes(i);
+                    HashSet<int> connected = vertex_graph.GetConnectedNodes(i);
 
-					XYZ avg = new XYZ(0, 0, 0);
-					foreach (int id in connected)
-					{
-						avg = avg + nodes[id];
-					}
+                    XYZ avg = new XYZ(0, 0, 0);
+                    foreach (int id in connected)
+                    {
+                        avg = avg + nodes[id];
+                    }
 
-					avg = avg / connected.Count;
+                    avg = avg / connected.Count;
 
-					smooth_nodes[i] = avg;
-				}
+                    smooth_nodes[i] = avg;
+                }
 
-				Array.Copy(smooth_nodes, nodes, smooth_nodes.Length);
-			}
-			
+                Array.Copy(smooth_nodes, nodes, smooth_nodes.Length);
+            }
 
-			GraphXYZ smooth_graph = new GraphXYZ();
-			smooth_graph.TryAddNodes(smooth_nodes);
-			
-			foreach (iEdge e in vertex_graph.GetEdgesConnectivity()) { smooth_graph.TryAddEdge(e.id_a, e.id_b); }
 
-			return smooth_graph;
-		}
-	}
+            GraphXYZ smooth_graph = new GraphXYZ();
+            smooth_graph.TryAddNodes(smooth_nodes);
+
+            foreach (iEdge e in vertex_graph.GetEdgesConnectivity()) { smooth_graph.TryAddEdge(e.id_a, e.id_b); }
+
+            return smooth_graph;
+        }
+    }
 }
 
